e94841c884f293477f8680ad9c23b044316aa7a4
tanvimagdum <magdum.t@northeastern.edu>
2024-02-01 15:38:07 -0500

    manipulation dom

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,29 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createButtonNode(btnTxt, e, l) {
+  let btnNode = document.createElement("button");
+  let buttonNodeTxt = createTxtNode(btnTxt);
+  btnNode.appendChild(buttonNodeTxt);
+  btnNode.addEventListener(e, l);
+  return btnNode;
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(createButtonNode("Edit text", "click", function() {
+      edit(col1);
+    }));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
+}
+
+function edit(node) {
+  let newnode = document.createElement("input");
+  newnode.type = 'text';
+  newnode.placeholder = 'Enter Cell (x,y) ...';
+  node.replaceChild(newnode, node.childNodes[0]);
 }
